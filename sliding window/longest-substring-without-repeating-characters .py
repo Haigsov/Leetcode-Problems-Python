56456322ab9045aa9a57ace213a4a273baed6190
Haigsov <haigsouv@my.yorku.ca>
2024-08-26 22:35:22 -0400

    Update longest-substring-without-repeating-characters .py

--- sliding window/longest-substring-without-repeating-characters .py
+++ sliding window/longest-substring-without-repeating-characters .py
@@ -1,9 +1,23 @@
 def lengthOfLongestSubstring(self, s):
-    """
+    """"
     :type s: str
     :rtype: int
     """
+
+    char_set = set()
+    l = 0
+    res = 0
+
+    for r in range(len(s)):
+        while s[r] in char_set:
+            char_set.remove(s[l])
+            l += 1
+        char_set.add(s[r])
+        res = max(res, r - l + 1)
     
+    return res
+    """
+    old solution
     res = ""
     seq = ""
 
@@ -22,4 +36,4 @@ def lengthOfLongestSubstring(self, s):
         res = max(res, seq, key=len)
         
     
-    return len(res)
+    return len(res)"""
