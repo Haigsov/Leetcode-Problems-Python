b6d808bf829a67e71d67db731082b97ea67643f1
Haigsov <haigsouv@my.yorku.ca>
2025-01-06 23:59:17 -0500

    Update longest_substring_with_replacing.py

--- sliding window/longest_substring_with_replacing.py
+++ sliding window/longest_substring_with_replacing.py
@@ -5,5 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        
+        res = 0
+        for i in range(1, len(s)):
+            curr = s[i]
+            j = i + 1
+            while k > 0 or j < len(s) - 1:
+                curr += s[j]
+                if curr[0] != s[j]:
+                    k -= 1
+                j += 1
+            res = max(len(curr), len(res))
+            curr = s[i]
+    
+        return res
         
